9c56a77a7a3532f2643e1628e50089b846490d4b
Laurens Ermers <ltf.ermers@student.han.nl>
2024-03-18 18:32:37 +0100

    t

---
+++ startcode/src/main/antlr4/nl/han/ica/icss/parser/.antlr/ICSSListener.java
@@ -0,0 +1,19 @@
+// Generated from c:/Users/laure/OneDrive/Documenten/GitHub/icss2022-sep-LaurensErmers/startcode/src/main/antlr4/nl/han/ica/icss/parser/ICSS.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ICSSParser}.
+ */
+public interface ICSSListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ICSSParser#stylesheet}.
+	 * @param ctx the parse tree
+	 */
+	void enterStylesheet(ICSSParser.StylesheetContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ICSSParser#stylesheet}.
+	 * @param ctx the parse tree
+	 */
+	void exitStylesheet(ICSSParser.StylesheetContext ctx);
+}
