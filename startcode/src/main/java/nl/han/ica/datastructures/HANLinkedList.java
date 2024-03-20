1f97c42a48faa27d3b5bb00224a9be8a70df7536
Laurens <74718420+Yskria@users.noreply.github.com>
2024-03-20 12:11:01 +0100

    added delete // started insert

--- startcode/src/main/java/nl/han/ica/datastructures/HANLinkedList.java
+++ startcode/src/main/java/nl/han/ica/datastructures/HANLinkedList.java
@@ -1,7 +1,5 @@
 package nl.han.ica.datastructures;
 
-import java.util.LinkedList;
-
 public class HANLinkedList<T> implements IHANLinkedList<T>{
 
     private ListNode<T> beginNode;
@@ -25,20 +23,39 @@ public class HANLinkedList<T> implements IHANLinkedList<T>{
     @Override
     public void insert(int index, T value) {
         ListNode<T> addNode = new ListNode<T>();
-        addNode.value = value;
-        //??????
-    }
-
-    @Override
-    public void delete(int pos) {
-        if(beginNode != null){
-            //??????
+        if(beginNode.value == null){
+            System.out.println("index node cannot be null");
+            if()
+        } else {
+            addNode.value = value;
         }
     }
 
     @Override
+    public void delete(int pos) {
+        LinkedListIterator<T> p = findPreviousNode(pos);
+        if(p.current.nextValue != null){
+            p.current.nextValue = p.current.nextValue.nextValue;
+        }
+    }
+
+    public LinkedListIterator<T> findPreviousNode(int pos){
+        ListNode<T> itr = beginNode;
+
+        while(itr.nextValue != null && itr.nextValue.value.equals(pos)){
+            itr = itr.nextValue;
+        }
+        return new LinkedListIterator<T>(itr);
+    }
+
+    @Override
     public T get(int pos) {
-        return null;
+        ListNode<T> llitr = beginNode.nextValue;
+
+        while (llitr != null && !llitr.value.equals(pos)){
+            llitr = llitr.nextValue;
+        }
+        return llitr.value;
     }
 
     @Override
--- startcode/src/main/java/nl/han/ica/datastructures/LinkedListIterator.java
+++ startcode/src/main/java/nl/han/ica/datastructures/LinkedListIterator.java
@@ -1,4 +1,22 @@
 package nl.han.ica.datastructures;
 
 public class LinkedListIterator<T> {
+    ListNode<T> current;
+    LinkedListIterator(ListNode<T> currentNode){
+        current = currentNode;
+    }
+
+    public boolean isValid(){
+        return current != null;
+    }
+
+    public T retrieve(){
+        return isValid() ? current.value : null;
+    }
+
+    public void advance(){
+        if(isValid()){
+            current = current.nextValue;
+        }
+    }
 }
